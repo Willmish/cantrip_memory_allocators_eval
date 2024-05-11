34b03ab34e9da90bd5b65aeab696c510a9307344
Szymon Duchniewicz <szduchniewicz@gmail.com>
2024-05-11 22:25:28 +0100

    WIP: update for pgf gen.
    Signed-off-by: Szymon Duchniewicz <szduchniewicz@gmail.com>

--- plot_memory_profiles.py
+++ plot_memory_profiles.py
@@ -1,8 +1,10 @@
 import json
 import matplotlib.pyplot as plt
+import matplotlib
 import argparse
 from pathlib import Path
 from os import makedirs
+import seaborn as sns
 
 def process_lines_from_file(file_path):
     with open(file_path, 'r') as file:
@@ -44,6 +46,30 @@ def process_lines_from_file(file_path):
     return data
 
 def plot_metrics(best_data, next_data, args: argparse.Namespace):
+    # Configure for PGF plots
+    matplotlib.rcParams.update(
+        {
+            "pgf.texsystem": "pdflatex",
+            "font.family": "serif",
+            "text.usetex": True,
+        }
+    )
+    matplotlib.use("pgf")
+    sns.set(style="whitegrid", font_scale=2)
+    plt.rcParams.update(
+    {
+        "text.color": "black",
+        "axes.edgecolor": "black",
+        "axes.labelcolor": "black",
+        "xtick.color": "black",
+        "ytick.color": "black",
+        "axes.linewidth": 1.5,
+        "xtick.major.size": 5,
+        "xtick.minor.size": 3,
+        "ytick.major.size": 5,
+        "ytick.minor.size": 3,
+    }
+)
     # Extracting data for plotting
     best_bytes_requested = [entry["bytes_requested"] for entry in best_data]
     best_bytes_in_use = [entry["bytes_in_use"] for entry in best_data]
@@ -65,41 +91,56 @@ def plot_metrics(best_data, next_data, args: argparse.Namespace):
     axs[0].set_xlabel('Bytes Requested')
     axs[0].set_title('Bytes in Use vs. Bytes Requested')
     axs[0].grid(True)
-    axs[0].legend()
 
-    # Plot for LHS Fragmentation
-    axs[1].plot(best_bytes_requested, best_lhs_fragmentation, label="Best Fit - LHS Fragmentation", marker=',', color='red')
+    # Plot for Watermarking Cons. Fragmentation
+    axs[1].plot(best_bytes_requested, best_lhs_fragmentation, label="Best Fit - Watermarking Cons. Fragmentation", marker=',', color='red')
     if args.separate_axis:
         ax2 = axs[1].twinx()
         color="tab:blue"
         ax2.tick_params(axis="y", labelcolor=color)
-        ax2.set_ylabel('Next Fit - LHS Fragmentation')
-        ax2.plot(next_bytes_requested, next_lhs_fragmentation, label="Next Fit - LHS Fragmentation", marker=',', color='red', linestyle="dotted")
-        axs[1].set_ylabel('Best Fit - LHS Fragmentation')
+        ax2.set_ylabel('Next Fit - Watermarking Cons. Fragmentation')
+        ax2.plot(next_bytes_requested, next_lhs_fragmentation, label="Next Fit - Watermarking Cons. Fragmentation", marker=',', color='red', linestyle="dotted")
+        axs[1].set_ylabel('Best Fit - Watermarking Cons. Fragmentation')
     else:
-        axs[1].plot(next_bytes_requested, next_lhs_fragmentation, label="Next Fit - LHS Fragmentation", marker=',', color='red', linestyle="dotted")
-        axs[1].set_ylabel('LHS Fragmentation')
+        axs[1].plot(next_bytes_requested, next_lhs_fragmentation, label="Next Fit - Watermarking Cons. Fragmentation", marker=',', color='red', linestyle="dotted")
+        axs[1].set_ylabel('Watermarking Cons. Fragmentation')
     axs[1].set_xlabel('Bytes Requested')
-    axs[1].set_title('LHS Fragmentation vs. Bytes Requested')
+    axs[1].set_title('Watermarking Cons. Fragmentation vs. Bytes Requested')
     axs[1].grid(True)
-    axs[1].legend()
 
-    # Plot for In-Between Fragmentation
-    axs[2].plot(best_bytes_requested, best_in_between_fragmentation, label="Best Fit - In-Between Fragmentation", marker=',', color='green')
+    # Plot for Alignment Cons. Fragmentation
+    axs[2].plot(best_bytes_requested, best_in_between_fragmentation, label="Best Fit - Alignment Cons. Fragmentation", marker=',', color='green')
     if args.separate_axis:
         ax2 = axs[2].twinx()
         color="tab:blue"
         ax2.tick_params(axis="y", labelcolor=color)
-        ax2.set_ylabel('Next Fit - In-Between Fragmentation')
-        ax2.plot(next_bytes_requested, next_in_between_fragmentation, label="Next Fit - In-Between Fragmentation", marker=',', color='green', linestyle="dotted")
-        axs[2].set_ylabel('Best Fit - In-Between Fragmentation')
+        ax2.set_ylabel('Next Fit - Alignment Cons. Fragmentation')
+        ax2.plot(next_bytes_requested, next_in_between_fragmentation, label="Next Fit - Alignment Cons. Fragmentation", marker=',', color='green', linestyle="dotted")
+        axs[2].set_ylabel('Best Fit - Alignment Cons. Fragmentation')
     else:
-        axs[2].plot(next_bytes_requested, next_in_between_fragmentation, label="Next Fit - In-Between Fragmentation", marker=',', color='green', linestyle="dotted")
-        axs[2].set_ylabel('In-Between Fragmentation')
+        axs[2].plot(next_bytes_requested, next_in_between_fragmentation, label="Next Fit - Alignment Cons. Fragmentation", marker=',', color='green', linestyle="dotted")
+        axs[2].set_ylabel('Alignment Cons. Fragmentation')
     axs[2].set_xlabel('Bytes Requested')
-    axs[2].set_title('In-Between Fragmentation vs. Bytes Requested')
+    axs[2].set_title('Alignment Cons. Fragmentation vs. Bytes Requested')
     axs[2].grid(True)
-    axs[2].legend()
+
+    # Merge all legends
+    handles0, labels0 = axs[0].get_legend_handles_labels()
+    handles1, labels1 = axs[1].get_legend_handles_labels()
+    handles2, labels2 = axs[2].get_legend_handles_labels()
+    plt.legend(
+        handles0 + handles1 + handles2,
+        labels0 + labels1 + labels2,
+        loc="upper center",
+        bbox_to_anchor=(0.5, -0.097),
+        ncol=3,
+        frameon=True,
+        fancybox=True,
+        edgecolor="black",
+    )
+    #axs[0].legend()
+    #axs[1].legend()
+    #axs[2].legend()
 
     plt.tight_layout()
     makedirs(args.output_dir, exist_ok=True)
@@ -107,6 +148,7 @@ def plot_metrics(best_data, next_data, args: argparse.Namespace):
         plt.savefig(Path(args.output_dir) / Path(f"SEP_{Path(args.best_fit_log_file).name.split('.')[-2]}.png"))
     else:
         plt.savefig(Path(args.output_dir) / Path(f"{Path(args.best_fit_log_file).name.split('.')[-2]}.png"))
+        plt.savefig(Path(args.output_dir) / Path(f"{Path(args.best_fit_log_file).name.split('.')[-2]}.pgf"), bbox_inches="tight")
     plt.show()
 
 def parse_args() -> argparse.Namespace:
