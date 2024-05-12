cb233dfb0f293b8c08aa89ff14a3cb53002d1663
Szymon Duchniewicz <szymon.duchniewicz.20@ucl.ac.uk>
2024-05-12 01:21:00 +0100

    Update plotting of memory profiles to produce pgf plots.
    Signed-off-by: Szymon Duchniewicz <szymon.duchniewicz.20@ucl.ac.uk>

--- plot_memory_profiles.py
+++ plot_memory_profiles.py
@@ -85,40 +85,40 @@ def plot_metrics(best_data, next_data, args: argparse.Namespace):
     fig, axs = plt.subplots(3, 1, figsize=(14, 21))
 
     # Plot for Bytes in Use
-    axs[0].plot(best_bytes_requested, best_bytes_in_use, label="Best Fit - Bytes in Use", marker=',', color='blue')
-    axs[0].plot(next_bytes_requested, next_bytes_in_use, label="Next Fit - Bytes in Use", marker=',', color='blue', linestyle="dotted")
+    axs[0].plot(best_bytes_requested, best_bytes_in_use, label="Best Fit - Bytes in Use", marker=',', color='blue', linewidth=3)
+    axs[0].plot(next_bytes_requested, next_bytes_in_use, label="Next Fit - Bytes in Use", marker=',', color='blue', linestyle="dotted", linewidth=3)
     axs[0].set_ylabel('Bytes in Use')
     axs[0].set_xlabel('Bytes Requested')
     axs[0].set_title('Bytes in Use vs. Bytes Requested')
     axs[0].grid(True)
 
     # Plot for Watermarking Cons. Fragmentation
-    axs[1].plot(best_bytes_requested, best_lhs_fragmentation, label="Best Fit - Watermarking Cons. Fragmentation", marker=',', color='red')
+    axs[1].plot(best_bytes_requested, best_lhs_fragmentation, label="Best Fit - Watermarking Con. Fragmentation", marker=',', color='red', linewidth=3)
     if args.separate_axis:
         ax2 = axs[1].twinx()
         color="tab:blue"
         ax2.tick_params(axis="y", labelcolor=color)
         ax2.set_ylabel('Next Fit - Watermarking Cons. Fragmentation')
-        ax2.plot(next_bytes_requested, next_lhs_fragmentation, label="Next Fit - Watermarking Cons. Fragmentation", marker=',', color='red', linestyle="dotted")
+        ax2.plot(next_bytes_requested, next_lhs_fragmentation, label="Next Fit - Watermarking Con. Fragmentation", marker=',', color='red', linestyle="dotted", linewidth=3)
         axs[1].set_ylabel('Best Fit - Watermarking Cons. Fragmentation')
     else:
-        axs[1].plot(next_bytes_requested, next_lhs_fragmentation, label="Next Fit - Watermarking Cons. Fragmentation", marker=',', color='red', linestyle="dotted")
+        axs[1].plot(next_bytes_requested, next_lhs_fragmentation, label="Next Fit - Watermarking Con. Fragmentation", marker=',', color='red', linestyle="dotted", linewidth=3)
         axs[1].set_ylabel('Watermarking Cons. Fragmentation')
     axs[1].set_xlabel('Bytes Requested')
     axs[1].set_title('Watermarking Cons. Fragmentation vs. Bytes Requested')
     axs[1].grid(True)
 
     # Plot for Alignment Cons. Fragmentation
-    axs[2].plot(best_bytes_requested, best_in_between_fragmentation, label="Best Fit - Alignment Cons. Fragmentation", marker=',', color='green')
+    axs[2].plot(best_bytes_requested, best_in_between_fragmentation, label="Best Fit - Alignment Con. Fragmentation", marker=',', color='green', linewidth=3)
     if args.separate_axis:
         ax2 = axs[2].twinx()
         color="tab:blue"
         ax2.tick_params(axis="y", labelcolor=color)
         ax2.set_ylabel('Next Fit - Alignment Cons. Fragmentation')
-        ax2.plot(next_bytes_requested, next_in_between_fragmentation, label="Next Fit - Alignment Cons. Fragmentation", marker=',', color='green', linestyle="dotted")
+        ax2.plot(next_bytes_requested, next_in_between_fragmentation, label="Next Fit - Alignment Con. Fragmentation", marker=',', color='green', linestyle="dotted", linewidth=3)
         axs[2].set_ylabel('Best Fit - Alignment Cons. Fragmentation')
     else:
-        axs[2].plot(next_bytes_requested, next_in_between_fragmentation, label="Next Fit - Alignment Cons. Fragmentation", marker=',', color='green', linestyle="dotted")
+        axs[2].plot(next_bytes_requested, next_in_between_fragmentation, label="Next Fit - Alignment Con. Fragmentation", marker=',', color='green', linestyle="dotted", linewidth=3)
         axs[2].set_ylabel('Alignment Cons. Fragmentation')
     axs[2].set_xlabel('Bytes Requested')
     axs[2].set_title('Alignment Cons. Fragmentation vs. Bytes Requested')
@@ -132,8 +132,10 @@ def plot_metrics(best_data, next_data, args: argparse.Namespace):
         handles0 + handles1 + handles2,
         labels0 + labels1 + labels2,
         loc="upper center",
-        bbox_to_anchor=(0.5, -0.097),
-        ncol=3,
+        # to avoid cutting into the text
+        borderpad=1,
+        bbox_to_anchor=(0.5, -0.120),
+        ncol=2,
         frameon=True,
         fancybox=True,
         edgecolor="black",
